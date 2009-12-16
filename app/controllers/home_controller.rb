4758d4eda5b730cba4f2de29279ac34f9bad4f95
Christoph Bünte <info@christophbuente.de>
2009-12-16 15:49:21 +0100

    added README, put some sample data to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,10 @@
 class HomeController < ApplicationController
+  
+  def index
+    if logged_in?
+      token = current_user.access_token(:dawanda)
+      @user = token.get('/api/v1/oauth/users/').body if token
+    end
+  end
+  
 end
