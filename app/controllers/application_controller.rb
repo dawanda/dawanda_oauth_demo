aca3e94e91803e1c658ee562c8b6ef3b6b8552eb
Sascha Depold <sascha@depold.com>
2009-12-18 14:36:06 +0100

    removed passwords from logging; helper method for detecting, if credentials are set; some styling and usibility stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  filter_parameter_logging "password"
   
   include AuthenticatedSystem
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
       
       if token
         @user = JSON.parse(token.get('/api/v1/oauth/users.json').body)["response"]["result"]["user"] rescue nil
-        @orders = JSON.parse(token.get("/api/v1/oauth/orders.json").body)["response"]["result"]["orders"] rescue nil
+        @orders = JSON.parse(token.get("/api/v1/oauth/orders.json").body)["response"]["result"]["orders"] rescue []
         date = Time.now.strftime("%Y/%m/%d")
         @orders_from_today = JSON.parse(token.get("/api/v1/oauth/orders.json?from=2009/12/17").body)["response"]["result"]["orders"] rescue nil
       end
